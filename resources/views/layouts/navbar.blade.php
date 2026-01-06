ecc9749d46f8b9410e41bcf160ffef079d2ff2e9
huda32 <mohnurulhuda32@gmail.com>
2026-01-06 13:48:11 +0700

    state

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -13,7 +13,7 @@
                     <a class="nav-link" href="/">Beranda</a>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link" href="/about">About</a>
+                    <a class="nav-link" href="/about">tentang</a>
                     </li>
                     <li class="nav-item">
                     <a class="nav-link" href="/contact">hubungi</a>
